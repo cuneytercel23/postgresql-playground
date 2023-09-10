d6d3fb155713e48367f14be279b7576c0fc2d800
cuneytercel23 <108323215+cuneytercel23@users.noreply.github.com>
2023-09-10 18:19:19 +0300

    sorting pagination filtre adding

--- controllers/book.js
+++ controllers/book.js
@@ -1,15 +1,44 @@
 const Book = require("../models/books");
 const User = require("../models/user");
+const { Op } = require("sequelize");
 
 class BookController {
   async list(req, res) {
+    //sorting
+    let { sort, sorttype } = req.query;
+    sort = sort ?? "name";
+    sorttype = sorttype ?? "ASC";
+    const sortObject = [sort, sorttype];
+
+    // pagination
+    let { page, limit } = req.query;
+    page = page ? parseInt(page) : 1;
+    limit = limit ? parseInt(size) : 10;
+    const offset = (page - 1) * limit;
+
+    // filtreleme
+    const filter = req.query.name ?? "za";
+
+    const whereClause = {
+      name: {
+        // name'e göre,
+        [Op.like]: `%${filter}%`,
+      },
+      // eksta bir id ye göre filtreleme yaparsam zaten basit where id : id ile.
+    };
+
     const book = await Book.findAll({
-        include:[
-         {
-            model:User,
-            as:"user"
-         }
-        ]
+      where: whereClause,
+      order: [sortObject], // sorting
+      offset: offset, // pagination
+      limit: limit, // pagination
+      include: [
+        // include
+        {
+          model: User,
+          as: "user",
+        },
+      ],
     });
     res.json(book);
   }
